c2d6b32822b333a2470325384e8957f3bbd46913
Alan <ala686876@gmail.com>
2026-03-17 12:42:11 +0500

    New convert method

--- utils/auth_ctx.go
+++ utils/auth_ctx.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/binary"
 	"lms_system/internal/domain/common"
 	"lms_system/internal/domain/entity"
 )
@@ -12,4 +14,24 @@ func GetUserFromContext(ctx context.Context) *entity.UserContext {
 		return nil
 	}
 	return userCtx
+}
+
+func ConvertKeycloakIDToUint(keycloakID string) uint {
+	if keycloakID == "" {
+		return 1 // Default fallback for testing
+	}
+
+	// Create MD5 hash of the Keycloak ID
+	hash := md5.Sum([]byte(keycloakID))
+
+	// Convert first 4 bytes to uint32, then to uint
+	// This ensures we get a consistent mapping for the same ID
+	id := binary.BigEndian.Uint32(hash[:4])
+
+	// Ensure we don't return 0
+	if id == 0 {
+		id = 1
+	}
+
+	return uint(id)
 }
